3cd9f9f524691f2404cbdea1db4f35efbde76c4c
William DURAND <william.durand1@gmail.com>
2012-11-30 19:00:03 +0100

    Add first working version

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,53 @@
-# = Class: stoplight
+# = Class: composer
 #
-class composer {
+# == Parameters:
+#
+# [*target_dir*]
+#   Where to install the composer executable.
+#
+# [*command_name*]
+#   The name of the composer executable.
+#
+# == Example:
+#
+#   include composer
+#
+#   class { 'composer':
+#     'target_dir'   => '/usr/local/bin',
+#     'command_name' => 'composer'
+#   }
+#
+class composer (
+  $target_dir   = 'UNDEF',
+  $command_name = 'UNDEF'
+) {
+
+  include composer::params
+
+  $composer_target_dir = $target_dir ? {
+    'UNDEF' => $::composer::params::target_dir,
+    default => $target_dir
+  }
+
+  $composer_command_name = $command_name ? {
+    'UNDEF' => $::composer::params::command_name,
+    default => $command_name
+  }
+
+  exec { 'composer-install':
+    command => "wget -O ${composer_command_name} ${::composer::params::phar_location}",
+    path    => '/usr/bin:/bin:/usr/sbin:/sbin',
+    cwd     => $composer_target_dir,
+    user    => 'root',
+    unless  => "test -f ${composer_target_dir}/${composer_command_name}",
+  }
+
+  exec { 'composer-fix-permissions':
+    command => "chmod a+x ${composer_command_name}",
+    path    => '/usr/bin:/bin:/usr/sbin:/sbin',
+    cwd     => $composer_target_dir,
+    user    => 'root',
+    unless  => "test -x ${composer_target_dir}/${composer_command_name}",
+    require => Exec['composer-install'],
+  }
 }
--- spec/classes/composer_spec.rb
+++ spec/classes/composer_spec.rb
@@ -3,4 +3,47 @@ require 'spec_helper'
 describe 'composer', :type => :class do
   let(:title) { 'composer' }
 
+  it { should contain_exec('composer-install') \
+    .with_command('wget -O composer http://getcomposer.org/composer.phar') \
+    .with_user('root') \
+    .with_cwd('/usr/local/bin')
+  }
+
+  it { should contain_exec('composer-fix-permissions') \
+    .with_command('chmod a+x composer') \
+    .with_user('root') \
+    .with_cwd('/usr/local/bin')
+  }
+
+  describe 'with a given target dir' do
+    let(:params) {{ :target_dir => '/usr/bin' }}
+
+    it { should contain_exec('composer-install') \
+      .with_command('wget -O composer http://getcomposer.org/composer.phar') \
+      .with_user('root') \
+      .with_cwd('/usr/bin')
+    }
+
+    it { should contain_exec('composer-fix-permissions') \
+      .with_command('chmod a+x composer') \
+      .with_user('root') \
+      .with_cwd('/usr/bin')
+    }
+  end
+
+  describe 'with a given command name' do
+    let(:params) {{ :command_name => 'c' }}
+
+    it { should contain_exec('composer-install') \
+      .with_command('wget -O c http://getcomposer.org/composer.phar') \
+      .with_user('root') \
+      .with_cwd('/usr/local/bin')
+    }
+
+    it { should contain_exec('composer-fix-permissions') \
+      .with_command('chmod a+x c') \
+      .with_user('root') \
+      .with_cwd('/usr/local/bin')
+    }
+  end
 end
